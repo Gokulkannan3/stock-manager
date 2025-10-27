eec56521d382cb0680e5cf3ceb9a416db833f4df
phoenixcrackers <phoenixcrackersfwc@gmail.com>
2025-10-27 23:36:57 +0530

    logout

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
   const username = localStorage.getItem('username');
-  return username ? <Outlet /> : <Navigate to="/admin" replace />;
+  return username ? <Outlet /> : <Navigate to="/" replace />;
 };
 
 export default ProtectedRoute;
